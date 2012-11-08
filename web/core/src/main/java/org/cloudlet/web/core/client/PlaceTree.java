bb047c77ea1b8d0ee642bb6ee434904f42edcdba
fengjian <computer-1@OverComputer>
2012-11-08 09:27:10 +0800

    merge

--- web/core/src/main/java/org/cloudlet/web/core/client/PlaceTree.java
+++ web/core/src/main/java/org/cloudlet/web/core/client/PlaceTree.java
@@ -2,26 +2,21 @@ package org.cloudlet.web.core.client;
 
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
-import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONValue;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import com.sencha.gxt.core.client.ValueProvider;
-import com.sencha.gxt.data.client.loader.HttpProxy;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.TreeStore;
 import com.sencha.gxt.data.shared.loader.ChildTreeStoreBinding;
 import com.sencha.gxt.data.shared.loader.DataReader;
 import com.sencha.gxt.data.shared.loader.TreeLoader;
-import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.VerticalLayoutData;
-import com.sencha.gxt.widget.core.client.event.SelectEvent;
-import com.sencha.gxt.widget.core.client.event.SelectEvent.SelectHandler;
-import com.sencha.gxt.widget.core.client.toolbar.ToolBar;
 import com.sencha.gxt.widget.core.client.tree.Tree;
 
 import org.cloudlet.web.core.shared.CorePackage;
@@ -31,6 +26,7 @@ import org.cloudlet.web.core.shared.WebPlace;
 import org.cloudlet.web.core.shared.WebPlaceManager;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PlaceTree extends BorderLayoutContainer {
@@ -39,23 +35,28 @@ public class PlaceTree extends BorderLayoutContainer {
 
     @Override
     public List<WebPlace> read(final Object loadConfig, final String data) {
+      WebPlace parentPlace = loadConfig == null ? rootPlace : (WebPlace) loadConfig;
       JSONObject root = JSONParser.parseLenient(data).isObject();
-      JSONObject feed = root.get("repository").isObject();
-      JSONArray children = feed.get(CorePackage.Content.CHILDREN).isArray();
-
-      List<WebPlace> result = new ArrayList<WebPlace>();
-      for (int i = 0; i < children.size(); i++) {
-        WebPlace place = placeProvider.get();
-        JSONObject object = children.get(i).isObject();
-        place.setPath(object.get("path").isString().stringValue());
-        place.setTitle(object.get("title").isString().stringValue());
-        String targetTypeName = object.get("@xsi.type").isString().stringValue();
-        PlaceType targetType = PlaceType.getType(targetTypeName);
-        place.setPlaceType(targetType);
-        rootPlace.addChild(place);
-        result.add(place);
+      JSONObject feed = root.get(parentPlace.getPlaceType().getName()).isObject();
+      JSONValue c = feed.get(CorePackage.Content.CHILDREN);
+      if (c != null) {
+        JSONArray children = c.isArray();
+        List<WebPlace> result = new ArrayList<WebPlace>();
+        for (int i = 0; i < children.size(); i++) {
+          WebPlace place = placeProvider.get();
+          JSONObject object = children.get(i).isObject();
+          place.setPath(object.get("path").isString().stringValue());
+          place.setTitle(object.get("title").isString().stringValue());
+          String targetTypeName = object.get("@xsi.type").isString().stringValue();
+          PlaceType targetType = PlaceType.getType(targetTypeName);
+          place.setPlaceType(targetType);
+          parentPlace.addChild(place);
+          result.add(place);
+        }
+        return result;
+      } else {
+        return Collections.EMPTY_LIST;
       }
-      return result;
     }
   }
 
@@ -81,10 +82,7 @@ public class PlaceTree extends BorderLayoutContainer {
 
     JSONFeedReader reader = new JSONFeedReader();
 
-    RequestBuilder rb =
-        new RequestBuilder(RequestBuilder.GET, "api/?" + CorePackage.Content.CHILDREN + "=true");
-    rb.setHeader("Accept", "application/json");
-    HttpProxy<WebPlace> jsonProxy = new HttpProxy<WebPlace>(rb);
+    PlaceProxy jsonProxy = new PlaceProxy(rootPlace);
     final TreeLoader<WebPlace> loader = new TreeLoader<WebPlace>(jsonProxy, reader) {
       @Override
       public boolean hasChildren(final WebPlace parent) {
@@ -121,26 +119,6 @@ public class PlaceTree extends BorderLayoutContainer {
       }
     });
 
-    ToolBar buttonBar = new ToolBar();
-
-    buttonBar.add(new TextButton("Expand All", new SelectHandler() {
-
-      @Override
-      public void onSelect(final SelectEvent event) {
-        loader.load();
-        // tree.expandAll();
-      }
-    }));
-
-    buttonBar.add(new TextButton("Collapse All", new SelectHandler() {
-      @Override
-      public void onSelect(final SelectEvent event) {
-        tree.collapseAll();
-      }
-
-    }));
-
-    add(buttonBar, new VerticalLayoutData(1, -1));
     add(tree, new VerticalLayoutData(1, 1));
   }
 }
