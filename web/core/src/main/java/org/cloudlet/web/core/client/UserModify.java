0dedf5ecf552a46b5affd909a0ab007b4d18d20a
fengjian <computer-1@OverComputer>
2012-11-19 10:55:03 +0800

    merge

--- web/core/src/main/java/org/cloudlet/web/core/client/UserModify.java
+++ web/core/src/main/java/org/cloudlet/web/core/client/UserModify.java
@@ -1,12 +1,15 @@
 package org.cloudlet.web.core.client;
 
 import com.google.gwt.event.logical.shared.AttachEvent;
-import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.json.client.JSONObject;
 import com.google.web.bindery.requestfactory.shared.RequestFactory;
 
 import com.sencha.gxt.widget.core.client.event.SelectEvent;
 
-import org.cloudlet.web.core.shared.WebPlace;
+import org.cloudlet.web.core.client.RequestBuilderBase.Callback;
+import org.cloudlet.web.core.shared.Feed;
+import org.cloudlet.web.core.shared.User;
 
 public class UserModify extends AbstractUserFieldView {
 
@@ -16,27 +19,32 @@ public class UserModify extends AbstractUserFieldView {
 
   @Override
   protected void onAttach(final AttachEvent event) {
-    // if (place == null) {
-    // place = placeManager.getWhere();
-    // }
-    send(initRequestBuilder(RequestBuilder.GET, "api" + placeManager.getWhere().getUri(), "Accept",
-        RequestFactory.JSON_CONTENT_TYPE_UTF8, new Responsecallback() {
-
-          @Override
-          public void completed(final String text) {
-            initJSON(text, true);
-          }
-        }), null);
+    try {
+      RequestBuilderBase.GET("api" + placeManager.getWhere().getUri()).accept(
+          RequestFactory.JSON_CONTENT_TYPE_UTF8).callback(new Callback<User>() {
+        @Override
+        public void onSuccess(JSONObject json) {
+          initJSON(json);
+        }
+      }).send();
+    } catch (RequestException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
-  protected RequestBuilder selectHandler(final SelectEvent event) {
-    return initRequestBuilder(RequestBuilder.PUT, "api" + placeManager.getWhere().getUri(),
-        "Content-Type", RequestFactory.JSON_CONTENT_TYPE_UTF8, new Responsecallback() {
-          @Override
-          public void completed(final String text) {
-            placeManager.goTo(place.getParent().getParent(), WebPlace.HOME);
-          }
-        });
+  protected void selectHandler(final SelectEvent event) {
+    try {
+      RequestBuilderBase.PUT("api" + placeManager.getWhere().getUri()).requestData(initJSON(null),
+          User.TYPE_NAME).contentType(RequestFactory.JSON_CONTENT_TYPE_UTF8).callback(
+          new Callback<User>() {
+            @Override
+            public void onSuccess(JSONObject json) {
+              placeManager.goTo(place.getParent().getParent(), Feed.LIST_WIDGET);
+            }
+          }).send();
+    } catch (RequestException e) {
+      e.printStackTrace();
+    }
   }
 }
