d435dd59a632b27069dc012e844192cb2e8f1042
Frank <dev@goodow.com>
2012-12-27 01:41:54 +0800

    filters

--- web/core/src/main/java/org/cloudlet/web/core/servlet/WebSecurityModule.java
+++ web/core/src/main/java/org/cloudlet/web/core/servlet/WebSecurityModule.java
@@ -1,6 +1,7 @@
 package org.cloudlet.web.core.servlet;
 
 import com.google.inject.Key;
+import com.google.inject.Singleton;
 import com.google.inject.name.Names;
 
 import org.apache.shiro.guice.web.ShiroWebModule;
@@ -9,19 +10,26 @@ import org.cloudlet.web.core.server.OAuthInitializer;
 
 import javax.servlet.ServletContext;
 
+import io.buji.oauth.OAuthFilter;
 import io.buji.oauth.OAuthRealm;
 import io.buji.oauth.filter.OAuthRolesAuthorizationFilter;
 
 public class WebSecurityModule extends ShiroWebModule {
 
-  @SuppressWarnings({ "UnusedDeclaration" })
+  public static final Key<OAuthFilter> OAUTH = Key.get(OAuthFilter.class);
+
   public static final Key<OAuthRolesAuthorizationFilter> GOOGLE_ROLES = Key.get(OAuthRolesAuthorizationFilter.class, Google.class);
 
-  @SuppressWarnings({ "UnusedDeclaration" })
   public static final Key<OAuthRolesAuthorizationFilter> YAHOO_ROLES = Key.get(OAuthRolesAuthorizationFilter.class, Yahoo.class);
 
   public WebSecurityModule(final ServletContext sc) {
     super(sc);
+  }
+
+  @Override
+  public void configure() {
+    super.configure();
+    bind(OAuthRealm.class).in(Singleton.class);
   }
 
   @Override
@@ -31,11 +39,14 @@ public class WebSecurityModule extends ShiroWebModule {
     // addFilterChain("/stuff/forbidden/**", AUTHC_BASIC, config(PERMS, "no"));
     // addFilterChain("/**", AUTHC_BASIC);
 
-    addFilterChain("/google/**", GOOGLE_ROLES);
-    addFilterChain("/yahoo/**", YAHOO_ROLES);
+    addFilterChain("/google/**", config(GOOGLE_ROLES, "ROLE_USER"));
+    addFilterChain("/yahoo/**", config(YAHOO_ROLES, "ROLE_USER"));
+    addFilterChain("/shiro-oauth", OAUTH);
+    addFilterChain("/logout", LOGOUT);
+    addFilterChain("/**", ANON);
 
     bindRealm().to(JpaRealm.class);
-    bindRealm().to(OAuthRealm.class);
+    bindRealm().to(OAuthRealm.class).in(Singleton.class);
     bind(OAuthInitializer.class).asEagerSingleton();
     bindConstant().annotatedWith(Names.named("shiro.globalSessionTimeout")).to(30000L);
   }
